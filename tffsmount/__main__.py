9a91c83415addb3b45ece2d544134293216ded95
Jonathan Herrewijnen <jonathan.herrewijnen@gmail.com>
2025-08-05 19:04:59 +0200

    adds simple function to parse key

--- tffsmount/__main__.py
+++ tffsmount/__main__.py
@@ -4,9 +4,17 @@ from argparse import ArgumentParser
 from pathlib import Path
 from tffsmount.mount import mount
 from tffsmount.logger import setup_logging
-
+import ast
 
 LOGGER = logging.getLogger("tffsmount")
+
+def parse_keys(path: Path):
+    data = path.read_text().splitlines()
+    result = {}
+    for i in range(len(data)):
+        result.update({i: ast.literal_eval(data[i])})
+        result[i]['key'] = result[i]['key'].hex()
+    return result
 
 
 def main(args):
@@ -20,9 +28,13 @@ if __name__ == "__main__":
     parser.add_argument("image", type=Path, help="Path to image containing tffs file system")
     parser.add_argument("mount_point", type=Path, help="Path to mount point")
     parser.add_argument("-o", "--offset", type=lambda x: int(x, 0), help="Offset of tffs partition in image", default=0)
+    parser.add_argument("-f", "--keys", type=Path, help="path to file with keys and key descriptors", default=None)
     args = parser.parse_args()
 
     setup_logging(LOGGER, loglevel=logging.DEBUG)
+
+    if args.keys is not None and args.keys.exists():
+        args.key_desc = parse_keys(args.keys)
 
     if not args.image.exists():
         LOGGER.info(f"Image file {args.image} not found, exiting.")
